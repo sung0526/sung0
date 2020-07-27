570aff2cf9fda4027b65a22e6796834469a105d7
sungyoung526 <sungyoung526@gmail.com>
2020-07-27 16:07:37 +0900

    비밀정보 보호

---
+++ config/prod.js
@@ -0,0 +1,4 @@
+module.exports = {
+    // mongoURI: process.env.MONGO_URI
+    mongoURI: 'mongodb+srv://sung0:Shtjddud77%21@sung0.kbjkk.mongodb.net/<sung0>?retryWrites=true&w=majority'
+}
--- index.js
+++ index.js
@@ -1,14 +1,37 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+const config = require("./config/key");
+//application/x-www-form-urlencoded 이렇게 된 데이터를 분석해서 가져올 수 있게 해주기 위한것
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json 타입으로 된 데이터를 분석해서 가져올 수 있게 해주기 위한것
+app.use(bodyParser.json());
+
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://sung0:Shtjddud77!@sung0.kbjkk.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log("MongoDB Connected..."))
     .catch(err => console.log(err))
 
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World! ddd'))
+
+app.post('/register', (req, res) => {
+    //회원 가입 할때 필요한 정보들을 client에서 가져오면 그것들을 데이터 베이스에 넣어준다
+
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
